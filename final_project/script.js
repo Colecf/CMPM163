12ae615d9cee88584635a670524f3dee9f03ea31
Cole Faust <colecfaust@gmail.com>
2018-03-09 13:45:47 -0800

    added box to scene

--- final_project/script.js
+++ final_project/script.js
@@ -4,10 +4,11 @@ var textures = {};
 
 load();
 function load() {
-    var todo = 2;
+    var shaderNames = ['scene.vert', 'scene.frag'];
+    var todo = shaderNames.length;
 
     var fileLoader = new THREE.FileLoader();
-    ['scene.vert', 'scene.frag'].forEach(name => {
+    shaderNames.forEach(name => {
         fileLoader.load(name, data => {
             shaders[name] = data;
             if(--todo == 0) {
@@ -19,18 +20,35 @@ function load() {
 
 function init() {
     scene = new THREE.Scene();
+
+    var light1 = new THREE.PointLight(0xff0000, 1, 0);
+    light1.position.set(0.0, 5.0, 5.0);
+    scene.add(light1);
+    var light2 = new THREE.PointLight(0x0000ff, 1, 0);
+    light2.position.set(0.0, -5.0, 5.0);
+    scene.add(light2);
+    var basicMat = new THREE.MeshPhongMaterial({});
+
+    var box = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), basicMat);
+    box.rotation.x = 20;
+    box.rotation.z = 20;
+    scene.add(box);
     
     renderer = new THREE.WebGLRenderer();
     renderer.setClearColor( 0x999999 );
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
-    camera = new THREE.OrthographicCamera( -0.5, 0.5, 0.5, -0.5, 0.1, 1000 );
-    camera.position.z = 0.3;
+    camera = new THREE.PerspectiveCamera( 60.0, window.innerWidth / window.innerHeight, 0.1, 50 );
+    camera.position.z = 5;
+
+    //camera = new THREE.OrthographicCamera( -1.5, 1.5, 1.5, -1.5, 0.1, 1000 );
+    //camera.position.z = 0.3;
 
     window.addEventListener( 'resize', onWindowResize, false );
 
     var gui = new dat.GUI();
     //gui.add(plane.material.uniforms.timeScale, "value", 0, 0.01).name("Time scale");
+    
     
     animate();
 }
